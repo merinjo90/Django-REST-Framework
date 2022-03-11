3c518900525bfcba5da8f1f2efba8c750a8bfc0f
merinjo90 <merinjo90@gmail.com>
2022-03-11 12:36:12 +0530

    blog list and BlogDetails view created

--- 3_Blog_API/blog_api/blog/views.py
+++ 3_Blog_API/blog_api/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+
+class BlogList(generics.ListAPIView):
+    queryset = models.Blog.objects.all()
+    serializer_class = BlogSerializer
+
+class BlogDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Blog.objects.all()
+    serializer_class = BlogSerializer
